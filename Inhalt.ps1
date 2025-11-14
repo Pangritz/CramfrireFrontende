8f10486eb24e85e7df8e7943e83492455a377155
Stefan Pangritz <pangritz@gvp-bonn.de>
2025-11-14 13:05:56 +0100

    Projektstart

---
+++ Inhalt.ps1
@@ -0,0 +1,12 @@
+﻿# Inhalt.ps1
+# Listet alle Ordner und Dateien im Verzeichnis, in dem dieses Skript liegt,
+# und speichert das Ergebnis in "Inhalt.txt".
+
+# Verzeichnis des Skripts ermitteln
+$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Ausgabedatei festlegen
+$OutFile = Join-Path $ScriptDir "Projektstruktur.txt"
+
+# Baumstruktur mit Dateien erzeugen
+tree $ScriptDir /F /A | Out-File $OutFile -Encoding UTF8
